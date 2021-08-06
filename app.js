32e98b7f7b5016dfa10d12de6b70a242e4655972
SebastianDarie <s.d.01.2002@gmail.com>
2021-08-06 19:40:51 +0300

    mdn express tut part 6

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
+
+app.use(helmet());
 
 var mongoose = require('mongoose');
 var mongoDB =
@@ -25,6 +29,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
